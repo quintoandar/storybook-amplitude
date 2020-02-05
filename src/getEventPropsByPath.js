21cb1b946d602e576d096ce29f9d1cb539a6210e
fernahh <fernahh@gmail.com>
2020-02-05 15:36:49 -0300

    Add try..catch to regex functions

--- src/getEventPropsByPath.js
+++ src/getEventPropsByPath.js
@@ -7,22 +7,39 @@ export default function getEventPropsByPath(path) {
 }
 
 function getGroup(path) {
-  const groupRegex = /(?<=\/(docs|story)\/)(.*)(?=--)/;
-  const splittedPath = groupRegex.exec(path)[0].split('-');
-  const pageIndex = splittedPath.length - 1;
-  splittedPath.splice(pageIndex, 1);
-  return splittedPath.join('-');
+  try {
+    const groupRegex = /(?<=\/(docs|story)\/)(.*)(?=--)/;
+    const splittedPath = groupRegex.exec(path)[0].split('-');
+    const pageIndex = splittedPath.length - 1;
+    splittedPath.splice(pageIndex, 1);
+    return splittedPath.join('-');
+  } catch (error) {
+    buildError('group', error);
+  }
 }
 
 function getPage(path) {
-  const groupRegex = /(\/.+?)(?=--)/;
-  const splittedPath = groupRegex.exec(path)[0].split('-');
-  const pageIndex = splittedPath.length - 1;
-  return splittedPath[pageIndex];
+  try {
+    const groupRegex = /(\/.+?)(?=--)/;
+    const splittedPath = groupRegex.exec(path)[0].split('-');
+    const pageIndex = splittedPath.length - 1;
+    return splittedPath[pageIndex];
+  } catch (error) {
+    buildError('page', error);
+  }
 }
 
 function getStory(path) {
-  const splittedPath = path.split('--');
-  const pageIndex = splittedPath.length - 1;
-  return splittedPath[pageIndex] === 'page' ? null : splittedPath[pageIndex];
+  try {
+    const splittedPath = path.split('--');
+    const pageIndex = splittedPath.length - 1;
+    return splittedPath[pageIndex] === 'page' ? null : splittedPath[pageIndex];
+  } catch (error) {
+    buildError('story', error);
+  }
+}
+
+function buildError(attr, error) {
+  console.error(`[storybook-amplitude] Error on get ${attr}`, error);
+  return null;
 }
--- test/getEventPropsByPath.test.js
+++ test/getEventPropsByPath.test.js
@@ -4,10 +4,28 @@ describe('Get event props by path', () => {
   const buildPath = (viewMode = 'docs') =>
     `/${viewMode}/components-fields-input--success`;
 
+  beforeEach(() => {
+    console.error = jest.fn();
+  });
+
+  afterEach(() => {
+    console.error.mockClear();
+  });
+
   it('should return the group name', () => {
     const path = buildPath();
     const event = getEventPropsByPath(path);
     expect(event.group).toBe('components-fields');
+  });
+
+  it('should log an error when try to get a group name from a invalid path', () => {
+    const path = '/invalid-group/components-fields-input--success';
+    const { group } = getEventPropsByPath(path);
+    expect(console.error).toBeCalledWith(
+      '[storybook-amplitude] Error on get group',
+      expect.any(TypeError)
+    );
+    expect(group).toBeUndefined();
   });
 
   it('should return the page name', () => {
@@ -16,10 +34,30 @@ describe('Get event props by path', () => {
     expect(event.page).toBe('input');
   });
 
+  it('should log an error when try to get a page name from a invalid path', () => {
+    const path = '/group/';
+    const { page } = getEventPropsByPath(path);
+    expect(console.error).toBeCalledWith(
+      '[storybook-amplitude] Error on get page',
+      expect.any(TypeError)
+    );
+    expect(page).toBeUndefined();
+  });
+
   it('should return the story name', () => {
     const path = buildPath();
     const event = getEventPropsByPath(path);
     expect(event.story).toBe('success');
+  });
+
+  it('should log an error when try to get a story name from a invalid path', () => {
+    const path = undefined;
+    const { story } = getEventPropsByPath(path);
+    expect(console.error).toBeCalledWith(
+      '[storybook-amplitude] Error on get story',
+      expect.any(TypeError)
+    );
+    expect(story).toBeUndefined();
   });
 
   it('should return events props from story mode', () => {
